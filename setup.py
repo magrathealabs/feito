c31f106e001aa2424fcd5ff4cde8a91e7284089a
Daniel Bastos <daniel.bastos1710@gmail.com>
2018-03-11 15:04:40 -0300

    Add DEVELOPMENT.md and split into dev and package requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup
 
 requires = [
     'requests==2.18.4',
-    'GitPython==2.1.8',
     'prospector==0.12.7'
 ]
 
 setup(
     name='feito',
-    version='0.0.4',
+    version='0.0.5',
     description='Automated code review in Python',
     url='http://github.com/magrathealabs/feito',
     author='Magrathea Labs',
